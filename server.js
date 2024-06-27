7be20826cc6678c8ce19c24842498cccba4b66da
ALi <ahamed.aaa01@gmail.com>
2024-06-27 18:30:38 +0530

    Server.js and DB configured

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+//Configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+
+//REST Object
+const app = express()
+
+//Middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+
+//REST API
+app.get('/', (req,res) => {
+    res.send('<h1>Welcome to Ecommerce app</h1>');
+});
+
+//PORT setup
+const PORT = process.env.PORT || 8081;
+
+//Run Listen
+app.listen(PORT, () => {
+    console.log(`Server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.brightBlue)
+});
