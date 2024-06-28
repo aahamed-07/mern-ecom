3f41932f2784db8eb5bccae762610afdd89c9e28
ALi <ahamed.aaa01@gmail.com>
2024-06-28 18:49:38 +0530

    MVC structure added and hashing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
 
 //Configure env
 dotenv.config();
@@ -18,7 +19,8 @@ const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 
-
+//routes
+app.use('/api/v1/auth', authRoutes);
 
 //REST API
 app.get('/', (req,res) => {
