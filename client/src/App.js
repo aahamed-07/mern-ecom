607522d8b32e72c19157fbc4941d3158a4554841
ALi <ahamed.aaa01@gmail.com>
2024-07-21 02:31:09 +0530

    Login Register added
    Login Register added partially with few  features still remaining.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import {Routes,Route} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Policy from './pages/Policy';
-import PageNotFound from './pages/PageNotFound';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PageNotFound from "./pages/PageNotFound";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<HomePage/>}></Route>
-      <Route path='/about' element={<About/>}></Route>
-      <Route path='/contact' element={<Contact/>}></Route>
-      <Route path='/policy' element={<Policy/>}></Route>
-      <Route path='/*' element={<PageNotFound/>}></Route> 
-      {/* Here '/*' means when no other routes work */}
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/policy" element={<Policy />}></Route>
+        <Route path="/*" element={<PageNotFound />}></Route>
+        {/* Here '/*' means when no other routes work */}
+      </Routes>
     </>
   );
 }
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoute.js';
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
 
 //Configure env
 dotenv.config();
@@ -11,20 +12,20 @@ dotenv.config();
 //database config
 connectDB();
 
-
 //REST Object
-const app = express()
+const app = express();
 
 //Middlewares
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 //routes
-app.use('/api/v1/auth', authRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 //REST API
-app.get('/', (req,res) => {
-    res.send('<h1>Welcome to Ecommerce app</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Ecommerce app</h1>");
 });
 
 //PORT setup
@@ -32,5 +33,8 @@ const PORT = process.env.PORT || 8081;
 
 //Run Listen
 app.listen(PORT, () => {
-    console.log(`Server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.brightBlue)
+  console.log(
+    `Server is running on ${process.env.DEV_MODE} mode on port ${PORT}`
+      .brightBlue
+  );
 });
