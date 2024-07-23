4671923b848fc204cdbfc94f418e66d82e2542d1
ALi <ahamed.aaa01@gmail.com>
2024-07-24 00:13:57 +0530

    Private Routes, Location History added
    Private Routes Location History and Redirect added.
    Minor design changes in footer

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -4,8 +4,8 @@ import { Link, link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <h4 className="text-center">All Rights Reserved &copy; aahamed-07</h4>
-      <p className="text-center mt-3">
+      <h5 className="text-center">All Rights Reserved &copy; aahamed-07</h5>
+      <p className="text-center mt-1">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/policy">Privacy Policy</Link>
       </p>
